e35929b2e805133f1f633c74b9919f0dddf959ce
pc-hover <133704411+pc-hover@users.noreply.github.com>
2023-06-11 14:38:58 +0530

    Update IntervalMerge.cpp

--- IntervalMerge.cpp
+++ IntervalMerge.cpp
@@ -43,27 +43,7 @@ public:
             }
             ans.push_back({start, end});
         }
-        for (int i = 0; i < n; i++)
-        {
-            int start = a[i][0];
-            int end = a[i][1];
-            if (!ans.empty() && end <= ans[ans.size() - 1][1])
-            {
-                continue;
-            }
-            for (int j = i + 1; j < n; j++)
-            {
-                if (end >= a[j][0])
-                {
-                    end = max(a[j][1], end);
-                }
-                else
-                {
-                    break;
-                }
-            }
-            ans.push_back({start, end});
-        }
+
         return ans;
     }
 };
