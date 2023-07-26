113ecf49d817f346cb2dc7a1c304d6104b157b01
pc-hover <133704411+pc-hover@users.noreply.github.com>
2023-07-26 18:57:02 +0530

    Add files via upload
    Amazing Problem:
    Sudoku solver is a  algorithm that can efficiently solve Sudoku puzzles, systematically filling in the missing numbers in a 9x9 grid to ensure that each row, column, and 3x3 subgrid contains all the digits from 1 to 9 without repetition.

---
+++ SudokoSolver.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void SudukoSolver(vector<vector<char>> &board)
+    {
+        Solve(board);
+    }
+    bool isSafe(vector<vector<char>> &board, int row, int col, char c)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[i][col] == c)
+            {
+                return false;
+            }
+            if (board[row][i] == c)
+            {
+                return false;
+            }
+
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool Solve(vector<vector<char>> &board)
+    {
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[0].size(); j++)
+            {
+                if (board[i][j] == '.')
+                {
+                    for (char c = '1'; c <= '9'; c++)
+                    {
+                        if (isSafe(board, i, j, c))
+                        {
+                            board[i][j] = c;
+
+                            if (Solve(board) == true)
+                            {
+                                return true;
+                            }
+
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void display(vector<vector<char>> &board)
+    {
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[0].size(); j++)
+            {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+int main()
+{
+    Solution s;
+    vector<vector<char>> SudokoBoard = {
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+    s.SudukoSolver(SudokoBoard);
+    s.display(SudokoBoard);
+    return 0;
+}
